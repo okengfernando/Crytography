ea186b54760079fd4351fad36bd65e17b8e819c4
fernando <okengfernando@github.com>
2021-02-23 17:41:00 +0300

    added more comprehensive tests for methods

--- tests.py
+++ tests.py
@@ -15,6 +15,14 @@ class Cipher_method_testers(unittest.TestCase):
         self.assertEqual(ciph.decrypt('wtaadldgas',15),'helloworld')
         self.assertEqual(ciph.decrypt('byffigsqilfx',20),'hellomyworld')
     
+    def test_decypt_false_result(self):
+        self.assertFalse(ciph.decrypt('rov',10) == 'hello')
+
+    def test_encrypt_false_result(self):
+        self.assertFalse(ciph.encrypt('hello',10) == 'rov')
+
+
+    
 
 if __name__ == "__main__":
     unittest.main()
